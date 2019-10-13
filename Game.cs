1a5df411b1a200304fbcbb4f36f436394fccdfd7
keith <khaizlett@gmail.com>
2019-10-13 22:16:08 +0200

    Finished game class, added new object in main method

--- Game.cs
+++ Game.cs
@@ -46,6 +46,39 @@ namespace Spaceman
             Console.WriteLine("Instructions: save your friend from alien abduction by guessing the letters in the codeword.");
         }
 
+        public void Display()
+        {
+            Console.WriteLine(spaceShip.Stringify());
+            Console.WriteLine($"Current word: {word}");
+            Console.WriteLine($"Wrong Guesses: {wrongNum}");
+            
+
+        }
+
+        public void Ask()
+        {
+            Console.WriteLine("Enter a letter.");
+          string stringGuess =   Console.ReadLine();
+            if (stringGuess.Trim().Length != 1)
+            {
+                Console.WriteLine("One letter at a time!");
+                return;
+            }
+            char guess = stringGuess.Trim().ToCharArray()[0];
+            if (Codeword.Contains(guess))
+            {
+                Console.WriteLine($"'{guess}' is in the word!");
+                for (int i = 0; i < Codeword.Length; i++)
+                {
+                    if (guess == Codeword[i])
+                    {
+                        word = word.Remove(i, 1).Insert(i, guess.ToString());
+                    }
+                }
+            }
+
+
+        }
         public bool didWin()
         {
 
@@ -57,8 +90,21 @@ namespace Spaceman
             {
                 return false;
             }
+        }
+            public bool didLose()
+            {
 
+                if (wrongNum >= maxNum)
+                {
 
+                    return true;
+
+                }
+                else
+                {
+
+                    return false;
+                }    
 
         }
 
--- Program.cs
+++ Program.cs
@@ -4,42 +4,30 @@ namespace Spaceman
 {
   class Program
   {
-        string Codeword= "";
-        string word = "";
-        int maxNum = 0;
-        int wrongNum = 0;
-        string[] codeword;
-
-        public  Program()
-        {
         
-
-            maxNum = 5;
-            wrongNum = 0;
-
-            for( int i =0; i < Codeword.Length; i++)
-            {
-                word += "_";
-            }
-
-            Console.WriteLine(word);
-        }
-
-        public static void Greet() {
-
-            Console.WriteLine("=============UFO: The Game =============");
-            Console.WriteLine("Instructions: save your friend from alien abduction by guessing the letters in the codeword.");
-
-        }
+   
+      
 
     static void Main(string[] args)
     {
-            Greet();
-            Ufo ufo = new Ufo();
-
-            Program p = new Program();
-            p.Codeword = "Happy";
-            Console.WriteLine(ufo);
+            do
+            {
+                Game g = new Game();
+                g.Greet();
+                g.Ask();
+                if (g.didLose())
+                {
+                    g.Display();
+                    Console.WriteLine("Oh no! The UFO just flew away with another person!");
+                    break;
+                }
+                else if (g.didWin())
+                {
+                    g.Display();
+                    Console.WriteLine("Hooray! You saved the person and earned a medal of honor!");
+                    break;
+                }
+            } while (true);
     }
   }
 }
